d014ca9b17ea0c97b06bd7abfc9c923799529e32
raja <raja@ge.com>
2018-04-29 20:28:59 +0530

    Created SQLAlchemy file and modified the mdoles

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from security import authenticate, identity
 from resources.user import UserRegister
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_TRCK_MODIFICATION'] = Flase
 app.secret_key = 'raja'
 api = Api(app)
 
@@ -19,4 +20,6 @@ api.add_resource(ItemList, '/items')
 api.add_resource(UserRegister, '/register')
 
 if __name__ == '__main__':
+    from db import db
+    db.init_app(app)
     app.run(port=5000, debug=True)
--- resources/item.py
+++ resources/item.py
@@ -1,6 +1,7 @@
 import sqlite3
 from flask_restful import Resource, reqparse
 from flask_jwt import JWT, jwt_required
+from models.item import ItemModel
 
 class Item(Resource):
     parser = reqparse.RequestParser()
@@ -12,72 +13,46 @@ class Item(Resource):
 
     #@jwt_required()
     def get(self, name):
-        item = self.find_by_name(name)
+        item = ItemModel.find_by_name(name)
         if item:
-            return item
+            return item.json()
         return {"message": "Item not found"}, 404
 
-    @classmethod
-    def find_by_name(cls, name):
-        connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()
 
-        query = "SELECT * FROM items WHERE name=?"
-        result = cursor.execute(query, (name,))
-        row = result.fetchone()
-        connection.close()
-        if row:
-            return {"item": {"name": row[0], "price": row[1]}}
 
     def post(self, name):
-        if self.find_by_name(name):
+        if ItemModel.find_by_name(name):
             return {'message': "An item with name '{}' already exists.".format(name)}, 400
         data = Item.parser.parse_args()
         print(data)
-        item = {'name': name, 'price': data['price']}
+        item = ItemModel(name, data['price'])
         try:
-            self.insert(item)
+            item.insert()
 
         except:
             return {"message": "An error occurred inserting an item"}, 500
-        return item, 201
+        return item.json(), 201
 
-    @classmethod
-    def insert(cls, item):
-        connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()
 
-        query = "INSERT INTO items VALUES (?, ?)"
-        cursor.execute(query, (item['name'], item['price']))
-        connection.commit()
-        connection.close()
 
     def put(self, name):
 
         data = Item.parser.parse_args()
-        item = self.find_by_name(name)
-        update_item = {'name': name, 'price': data['price']}
+        item = ItemModel.find_by_name(name)
+        update_item = ItemModel(name, data['price'])
         if item == None:
             try:
-                self.insert(update_item)
+                update_item.insert()
             except:
                 return {"message": "An error occurred while insert item"}, 500
         else:
             try:
-                self.update(update_item)
+                update_item.update()
             except:
                 return {"message": "An error occurred while update item"}, 500
-        return item
+        return update_item.json()
 
-    @classmethod
-    def update(cls, item):
-        connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()
 
-        query = "UPDATE items SET price=? WHERE name=?"
-        cursor.execute(query, (item['price'], item['name']))
-        connection.commit()
-        connection.close()
 
 
     def delete(self, name):
